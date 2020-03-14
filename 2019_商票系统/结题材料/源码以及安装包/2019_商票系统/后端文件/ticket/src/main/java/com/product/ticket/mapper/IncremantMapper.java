235f53d22fea0204d4c9a2613f6be238e7bfede9
weilllllls <1113887575@qq.com>
2020-03-14 10:30:29 +0800

    删除结题材料

--- 2019_商票系统/结题材料/源码以及安装包/2019_商票系统/后端文件/ticket/src/main/java/com/product/ticket/mapper/IncremantMapper.java
+++
@@ -1,9 +0,0 @@
-package com.product.ticket.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.product.ticket.entity.Increment;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface IncremantMapper extends BaseMapper<Increment> {
-}
--- 2019_商票系统/结题材料/源码以及安装包/2019_商票系统/后端文件/ticket/src/main/java/com/product/ticket/mapper/MessageMapper.java
+++
@@ -1,9 +0,0 @@
-package com.product.ticket.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.product.ticket.entity.Message;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MessageMapper extends BaseMapper<Message> {
-}

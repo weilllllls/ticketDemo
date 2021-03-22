84e5802cffb6cfdedadf754a4a41d7520226faf1
weilllllls <60218260+weilllllls@users.noreply.github.com>
2021-03-22 15:29:52 +0800

    Delete 2019_商票系统/结题材料 directory

--- 2019_商票系统/结题材料/⑥源码/2019_商票系统/后端文件/ticket/src/main/java/com/product/ticket/mapper/BankMapper.java
+++
@@ -1,9 +0,0 @@
-package com.product.ticket.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.product.ticket.entity.Bank;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BankMapper extends BaseMapper<Bank> {
-}
--- 2019_商票系统/结题材料/⑥源码/2019_商票系统/后端文件/ticket/src/main/java/com/product/ticket/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.product.ticket.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.product.ticket.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
